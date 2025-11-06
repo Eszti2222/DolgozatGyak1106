75ff450417b73e2e20de082cefd9077301479da5
Eszti2222 <nagy.eszter2222@gmail.com>
2025-11-06 15:01:37 +0100

    Ajto es ModelKincsesAjto

--- src/Model/Ajto.java
+++ src/Model/Ajto.java
@@ -3,16 +3,36 @@ package Model;
 public class Ajto {
 
     private boolean nyeremeny;
+    private boolean kivalasztott;
+    private boolean nyitott;
 
     public Ajto(boolean nyeremeny) {
         this.nyeremeny = nyeremeny;
+        this.kivalasztott = false;
+        this.nyitott = false;
     }
 
-    public boolean isnyeremeny() {
+    public boolean isNyeremeny() {
         return nyeremeny;
     }
 
     public void nyeremenyetElhelyez() {
         nyeremeny = true;
+    }
+
+    public boolean isKivalasztott() {
+        return kivalasztott;
+    }
+
+    public void setKivalasztott(boolean kivalasztott) {
+        this.kivalasztott = kivalasztott;
+    }
+
+    public boolean isNyitott() {
+        return nyitott;
+    }
+
+    public void setNyitott(boolean nyitott) {
+        this.nyitott = nyitott;
     }
 }
--- src/Model/ModelKincsesAjto.java
+++ src/Model/ModelKincsesAjto.java
@@ -1,15 +1,57 @@
 package Model;
 
+import java.util.Random;
+
 public class ModelKincsesAjto {
-    
+
     private Ajto[] ajtok;
-    private int valasztas;
-    
-     public ModelKincsesAjto() {
-        valasztas = -1;
+    private int valasztottAjto = -1;
+    private int nyitottAjto = -1;
+
+    public ModelKincsesAjto() {
         ajtok = new Ajto[3];
-        ajtok[0] = new Ajto(nyeremenyErtek);
-        ajtok[1] = new Ajto(true);
-        ajtok[2] = new Ajto();
+
+        for (int i = 0; i < 3; i++) {
+            ajtok[i] = new Ajto(false);
+        }
+
+        Random rand = new Random();
+        int nyeremenyIndex = rand.nextInt(3);
+        ajtok[nyeremenyIndex].nyeremenyetElhelyez();
+    }
+
+    public void valasztas(int ajtoIndex) {
+        this.valasztottAjto = ajtoIndex;
+        nyitottAjto = keresNyithatoAjto();
+    }
+
+    private int keresNyithatoAjto() {
+        for (int i = 0; i < 3; i++) {
+            if (i != valasztottAjto && !ajtok[i].isNyeremeny()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getNyitottAjto() {
+        return nyitottAjto;
+    }
+
+    public boolean ellenoriz(int vegsoValasztas) {
+        return ajtok[vegsoValasztas].isNyeremeny();
+    }
+    
+    public int getMasikAjto() {
+        for (int i = 0; i < 3; i++) {
+            if (i != valasztottAjto && i != nyitottAjto) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public int getValasztottAjto() {
+        return valasztottAjto;
     }
 }
